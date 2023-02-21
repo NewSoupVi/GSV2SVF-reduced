6bfd603b8ff724fd674c1f343b3ae88b1c56be31
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2023-02-21 11:45:22 +0100

    Replaced complicated core system with single .bat script

---
+++ RunTaskSimple.bat
@@ -0,0 +1,5 @@
+set SVFHOME=%~dp0
+set PYTHONHOME=%~dp0/Miniconda2
+set PYTHONPATH=%PYTHONHOME%Lib
+set PythonDir=%PYTHONHOME%
+%PYTHONHOME%\python.exe %SVFHOME%RunTaskSimple.py %1
---
+++ RunTaskSimple.py
@@ -0,0 +1,24 @@
+﻿import sys
+import os
+SVFHOME = os.environ['SVFHOME']
+sys.path.insert(0, SVFHOME + 'PyCaffeCUDA/pycaffe')
+import SVFCore
+CacheDir = SVFHOME + "Cache\\Default\\"
+import time
+
+
+if __name__ == "__main__":
+    dir = sys.argv[1]
+    GSVCapture = SVFCore.GSVCapture()
+    GSVCapture.initialize(True)
+    sys.stdout.flush()
+    try:
+        sys.stdout.write("task.started\n")
+        sys.stdout.flush()
+        GSVCapture.batchGetByID(dir)
+        sys.stdout.write("task.finished\n")
+        sys.stdout.flush()
+    except Exception as err:
+        sys.stdout.write(str(err) + "\n")
+        sys.stdout.write("task.failed\n")
+        sys.stdout.flush()
